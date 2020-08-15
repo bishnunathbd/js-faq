5fca3a48681daba23473b081a19886259a4656b3
bishnunathbd <bishnunath774@gmail.com>
2020-08-15 20:29:48 +0600

    difference between bind, call, apply

--- call-apply.js
+++ call-apply.js
@@ -5,9 +5,9 @@ const normalPerson = {
     getFullName: function () {
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function (amount) {
+    chargeBill: function (amount, tips, tax) {
         console.log(this);
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 }
@@ -25,11 +25,19 @@ const zeroPerson = {
 }
 
 
-const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
-heroChargeBill(2000);
-heroChargeBill(3000);
+// const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+// heroChargeBill(2000);
+// heroChargeBill(3000);
 // console.log(heroPerson.salary);
 // console.log(normalPerson.salary);
 
-const zeroChargeBill = normalPerson.chargeBill.bind(zeroPerson);
-zeroChargeBill(2000);
+// const zeroChargeBill = normalPerson.chargeBill.bind(zeroPerson);
+// zeroChargeBill(2000);
+
+// normalPerson.chargeBill.call(heroPerson, 3000, 300, 30);
+// console.log(heroPerson.salary);
+// normalPerson.chargeBill.call(zeroPerson, 3000, 100, 10);
+// console.log(zeroPerson.salary);
+
+normalPerson.chargeBill.apply(heroPerson, [3000, 300, 30]);
+console.log(heroPerson.salary);
