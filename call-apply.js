c9f5daaf83d188e58183f3b4262e7d35d5c4dcd3
bishnunathbd <bishnunath774@gmail.com>
2020-08-15 18:26:21 +0600

    use bind to borrow method from another object

--- call-apply.js
+++ call-apply.js
@@ -6,10 +6,30 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
     chargeBill: function (amount) {
+        console.log(this);
         this.salary = this.salary - amount;
         return this.salary;
     }
 }
 
-normalPerson.chargeBill(150);
-console.log(normalPerson.salary);
+const heroPerson = {
+    firstName: 'Hero',
+    lastName: 'Balam',
+    salary: 25000
+}
+
+const zeroPerson = {
+    firstName: 'Zero',
+    lastName: 'Bishnu',
+    salary: 10000
+}
+
+
+const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+heroChargeBill(2000);
+heroChargeBill(3000);
+// console.log(heroPerson.salary);
+// console.log(normalPerson.salary);
+
+const zeroChargeBill = normalPerson.chargeBill.bind(zeroPerson);
+zeroChargeBill(2000);
